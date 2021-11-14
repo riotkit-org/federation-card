531486a1d54b14d08b1db13a0745e971c19a1a39
Andrew Johnson <blackandred@users.noreply.github.com>
2021-11-14 23:29:17 +0100

    Fix formatting

--- content/_layouts/pages/organizations-single.blade.php
+++ content/_layouts/pages/organizations-single.blade.php
@@ -2,6 +2,6 @@
 
 @section('body')
     <div class="p-8">
-        <h1 class="text-3xl font-bold">{{ $page }}</h1>
+        <h1 class="text-3xl font-bold">{!! $page !!}</h1>
     </div>
 @endsection
