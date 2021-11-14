8a1ef7c32c837d0489dda03c152448ef8417be39
Andrew Johnson <blackandred@users.noreply.github.com>
2021-11-14 23:25:30 +0100

    Add baseUrl that match current environment that uses Github's default domain

--- config.production.php
+++ config.production.php
@@ -6,4 +6,6 @@ return [
         'source'      => 'content',
         'destination' => 'build_production',
     ],
+    // on dev environment the url is in a subfolder, as we use github's default domain
+    'baseUrl' => '/federation-card/'
 ];
